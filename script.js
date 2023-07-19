1dc2d2f8576eea9d757608d93a4d747ca9b6db0a
shubham3619 <120643298+shubham3619@users.noreply.github.com>
2023-07-20 02:50:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+	// const sizeInfoDiv = document.createElement("div");
+	// sizeInfoDiv.id = "sizeInfo";
+  const sizeInfoDiv = document.getElementById('sizeInfo');
+  const sizeText = document.createElement('h1');
+  sizeText.textContent = 'Width: ' + window.innerWidth + ' and Height: ' + window.innerHeight;
+  sizeInfoDiv.appendChild(sizeText);
+
+  window.addEventListener('resize', () => {
+    sizeText.textContent = 'Width: ' + window.innerWidth + ' and Height: ' + window.innerHeight;
+  });
+});
